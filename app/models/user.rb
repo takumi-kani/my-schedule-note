719e89653dd31df1b7af85f2d8617179db47e6e0
takumi-kani <hxjtp796@gmail.com>
2021-09-30 14:01:49 +0900

    モデルとコントローラーの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は「ひらがな」か「カタカナ」か「漢字」で入力してください' }
+    validates :password, format: { with: /\A[0-9]+\z/ , message: 'は半角数字のみで入力してください'}
+  end
+        
+  has_many :schedules
 end
