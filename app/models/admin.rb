b44d6a4cd2e20249cbf350f42be2e5b6301f0be6
takumi-kani <hxjtp796@gmail.com>
2021-09-30 14:05:28 +0900

    モデルとコントローラーの編集

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,16 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
+<<<<<<< Updated upstream
          :recoverable, :rememberable, :validatable
+=======
+         :recoverable, :rememberable
+
+  with_options presence: true do
+    validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は「ひらがな」か「カタカナ」か「漢字」で入力してください' }
+    validates :password, format: { with: /\A[0-9]+\z/ , message: 'は半角数字のみで入力してください'}
+  end
+
+  has_many :schedules
+>>>>>>> Stashed changes
 end
