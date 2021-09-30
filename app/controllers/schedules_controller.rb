47cab9d901dd92ecfd2e1e5acf16ae287b417107
takumi-kani <hxjtp796@gmail.com>
2021-09-30 14:41:24 +0900

    新規予定の作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,4 +2,27 @@ class SchedulesController < ApplicationController
   def index
     @schedules = Schedule.all
   end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to root_path(@schedule)
+    else
+      render new_schedule_path
+    end
+  end 
+
+  private
+
+  def schedule_params
+    if user_signed_in?
+      params.require(:schedule).permit(:title, :start_time, :end_time, :place, :info).merge(user_id: current_user.id)
+    else
+      params.require(:schedule).permit(:title, :start_time, :end_time, :place, :info).merge(admin_id: current_admin.id)
+    end
+  end
 end
