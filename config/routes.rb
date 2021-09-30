4ac515ff5a543db77da15fcaae069343653da35f
takumi-kani <hxjtp796@gmail.com>
2021-09-30 14:06:16 +0900

    カレンダー表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  root to: 'schedules#index'
+  
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  
+  resources :schedules
+>>>>>>> Stashed changes
 end
